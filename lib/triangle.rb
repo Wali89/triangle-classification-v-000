f12cf6ea21652a8b678cf17be8792cc8570579f6
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-09 01:52:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :side1, :side2, :side3, :type
 
   def initalize(side1:, side2:, side3:)
     @side1 = side1
@@ -8,7 +8,15 @@ class Triangle
   end
 
   def kind
-
+    if @side1.to_i == @side2.to_i == @side3.to_i
+      return :equilateral
+      
+    elsif @side1.to_i == @side2.to_i || @side2.to_i == @side3.to_i || @side1.to_i == @side3.to_i
+      return :isosceles
+       
+    elsif @side2.to_i == 
+      
+    end
   end
 end
 
