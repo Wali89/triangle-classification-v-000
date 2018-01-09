41e699c1b7ddd2e8f9e61b536076d517ba6ff18a
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-09 02:17:03 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,18 +7,18 @@ class Triangle
     @s3 = s3.to_i
   end
 
-  def kind(:s1, :s2, :s3)
-    if @s1 = 0 || @s2 = 0 || @s3 = 0
+  def kind
+    if s1 = 0 || s2 = 0 || s3 = 0
       raise TriangleError
-    elsif (@s1 + @s2) <= @s3 || (@s1 + @s3) <= @s2 || (@s2 + @s3) <= @s1
+    elsif (s1 + s2) <= s3 || (s1 + s3) <= s2 || (s2 + s3) <= s1
       raise TriangleError
-    elsif @s1 == @s2 == @s3
+    elsif s1 == s2 == s3
       @type =
 
-    elsif @s1 == @s2 || @s2 == @s3 || @s1 == @s3
+    elsif s1 == s2 || s2 == s3 || s1 == s3
       return :isosceles
 
-    elsif @s2 == @s3 || @s1 == @s2 || @s1 == @s3
+    elsif s2 == s3 || s1 == s2 || s1 == s3
 
     end
   end
