452238769e2a68ed29e924f349804477b944ea4a
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-09 02:10:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,21 +1,25 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3, :type
+  attr_accessor :s1, :s2, :s3, :type
 
-  def initalize(side1:, side2:, side3:)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  def initalize(s1:, s2:, s3:)
+    @s1 = s1.to_i
+    @s2 = s2.to_i
+    @s3 = s3.to_i
   end
 
-  def kind
-    if @side1.to_i == @side2.to_i == @side3.to_i
-      return :equilateral
-      
-    elsif @side1.to_i == @side2.to_i || @side2.to_i == @side3.to_i || @side1.to_i == @side3.to_i
+  def kind(@s1, @s2, @s3)
+    if @s1 = 0 || @s2 = 0 || @s3 = 0
+      raise TriangleError
+    elsif (@s1 + @s2) <= @s3 || (@s1 + @s3) <= @s2 || (@s2 + @s3) <= @s1
+      raise TriangleError 
+    elsif @s1 == @s2 == @s3
+      @type =  
+
+    elsif @s1 == @s2 || @s2 == @s3 || @s1 == @s3
       return :isosceles
-       
-    elsif @side2.to_i == 
-      
+
+    elsif @s2 == @s3 || @s1 == @s2 || @s1 == @s3
+
     end
   end
 end
@@ -23,4 +27,7 @@ end
 
 class TriangleError < StandardError
 
+  def message
+    ""
+  end
 end
