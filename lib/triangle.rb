8a13bf609fcb257158352ef9b501d1e6d35e46e6
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-09 02:37:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,31 +9,21 @@ class Triangle
 
   def kind
     validate_triangle
-    if self.s1 = 0 || self.s2 = 0 || self.s3 = 0
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    elsif (self.s1 + self.s2) <= self.s3 || (self.s1 + self.s3) <= self.s2 || (self.s2 + self.s3) <= self.s1
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    elsif self.s1 = self.s2 = self.s3
-      @type =
-
-    elsif self.s1 = self.s2 || self.s2  self.s3 || self.s1 = self.s3
-      return :isosceles
-
-    elsif self.s2 = self.s3 || self.s1 = self.s2 || self.s1 = self.s3
-
+    if s1 == s2 && s2 == s3
+      :equilateral
+    elsif s1 == s2 || s2 == s3 || s1 == s3
+      :isosceles
+    else
+      :scalene
     end
   end
-end
-end
 
+  def validate_triangle
+    real_triangle = [(s1 + s2 > s3), (s1 + s3 > s2), (s2 + s3 > s1)]
+    [s1, s2, s3].each { |s| real_triangle << false if s <= 0 }
+    raise TraingleError if real_triangle.include?(false)
+  end
+end
 
 class TriangleError < StandardError
 
